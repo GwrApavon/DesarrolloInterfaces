bef27e819063a764e76c52e6e3376922e3335b12
Ángel Pavón Fraile <bc.apavon.1995@iespabloserrano.com>
2022-09-26 11:50:46 +0200

    Prueba C#

---
+++ NavegadorWeb/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavegadorWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            if (url.Trim() == "")
+            {
+                url = "www.duckduckgo.com";
+            }
+            webBrowser1.Navigate(url);
+            this.Text = "Estamos en: " + url;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
