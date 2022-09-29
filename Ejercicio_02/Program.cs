e2151a829562bccae1afc1f51fc1ce1ba896d1be
Ángel Pavón Fraile <bc.apavon.1995@iespabloserrano.com>
2022-09-29 10:04:57 +0200

    Ejercicio 02 Frases

---
+++ Ejercicio_02/Program.cs
@@ -0,0 +1,11 @@
+﻿//#define FRASE
+Console.WriteLine("Introduce una frase:");
+string frase1 = Console.ReadLine();
+Console.WriteLine("Introduce otra frase:");
+string frase2 = Console.ReadLine();
+
+#if FRASE
+Console.WriteLine("\"{0}\"\t \\{1}\\ ", frase1, frase2);
+#else
+Console.WriteLine("\n{0}\t{1}\n{0}\n{1}", frase1,frase2);
+#endif
