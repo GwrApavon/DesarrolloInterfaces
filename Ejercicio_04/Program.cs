00babbb36f20c3a76eadbdfef079df1dae63d3bf
Ángel Pavón Fraile <bc.apavon.1995@iespabloserrano.com>
2022-10-19 09:36:51 +0200

    Ejercicio 4 terminado

--- Ejercicio_04/Program.cs
+++ Ejercicio_04/Program.cs
@@ -4,82 +4,85 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Boolean salir = false;
-        do
-        {
-            Console.WriteLine("1. Juego 1 ");
-            Console.WriteLine("2. Juego 2");
-            Console.WriteLine("3. Juego 3");
-            Console.WriteLine("4. Todos");
-            Console.WriteLine("0. Salir ");
-            int op = int.Parse(Console.ReadLine());
-            switch (op)
+             bool ejecutar = false;
+             int top = 0;
+             int left = 0;  
+             for (int k = 0; ; )
             {
-                case 1:
-                    Boolean salir1 = true;
-                    do
-                    {
-                        Juego1();
-
-                        Console.WriteLine("Quiere volver a jugar? Si/No");
-                        if ( Console.ReadLine().ToUpper().Equals("no".ToUpper()))
-                        {
-                            salir1 = false;
-                        }
-
-                    } while(salir1);
-                   
-                    if(op == 4)
-                    {
-                        goto case 2;
-                    }
-                    break;
-              
-                case 2:
-                    Boolean salir2 = true;
-                    do
-                    {
-                        Juego2();
-
-                        Console.WriteLine("Quiere volver a jugar? Si/No");
-                        if (Console.ReadLine().ToUpper().Equals("no".ToUpper()))
-                        {
-                            salir2 = false;
-                        }
-
-                    } while (salir2);
-                   
-                    if (op == 4)
-                    {
-                        goto case 3;
-                    }
-                    break;
-               
-                case 3:
-                    Boolean salir3 = true;
-                    do
-                    {
-                        Juego3();
-
-                        Console.WriteLine("Quiere volver a jugar? Si/No");
-                        if (Console.ReadLine().ToUpper().Equals("no".ToUpper()))
-                        {
-                            salir3 = false;
-                        }
-
-                    } while (salir3);
-                    break;
+                DibujarCuadro();
+                DibujarOpciones(k);
+                ConsoleKeyInfo cki = Console.ReadKey(true);
                 
-                case 4:
-                    goto case 1;
-                
-                case 0:
-                    salir = true;
-                    break;
-            }
-        } while (!salir);
-        
+                switch (cki.Key)
+                {
+                    case ConsoleKey.UpArrow: k--; break;
+                    case ConsoleKey.DownArrow: k++; break;
+                    case ConsoleKey.Enter: ejecutar = true; break;
+                }
+
+                if (k < 0) k = 4; 
+                else if (k > 4) k = 0;
+
+                if (ejecutar)
+                {
+                    ejecutar = false;
+                    switch (k)
+                    {
+                        //JUEGO 1
+                        case 0:
+                            do
+                            {
+                                Juego1();
+                                top = Console.GetCursorPosition().Top;
+                                left = Console.GetCursorPosition().Left;      
+                                
+                            } while(Retorno(left, top));
+                            Console.Clear();
+                   
+                            if(k == 4)
+                            {
+                                goto case 1;
+                            }
+                            break;
+
+                        //JUEGO 2
+                        case 1:
+                            do
+                            {
+                                Juego2();
+                                top = Console.GetCursorPosition().Top;
+                                left = Console.GetCursorPosition().Left;
+                            } while (Retorno(left, top));
+                            Console.Clear();
+                            if (k == 4)
+                            {
+                                goto case 2;
+                            }
+                            break;
+                        
+                        //JUEGO 3
+                        case 2:
+                            do
+                            {
+                                Juego3();
+                                top = Console.GetCursorPosition().Top;
+                                left = Console.GetCursorPosition().Left;
+                            } while (Retorno(left, top));
+                            Console.Clear();
+                            break;
+                        
+                        //TODOS
+                        case 3:
+                            goto case 0;
+
+                        //SALIR
+                        case 4:
+                            return;
+                    }
+                }
+             }
     }
+
     //Juego 1
         public static void Juego1()
         {
@@ -187,9 +190,87 @@ public class Program
         //Dibuja el cuadro
 
         public static void DibujarCuadro()
+        {   
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(2,2);
+            Console.WriteLine("+------------------------------------------------+"); //51 - 2
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.SetCursorPosition(2,2+i);
+                Console.WriteLine("|");
+                Console.SetCursorPosition(51,2+i);
+                Console.WriteLine("|");
+            }
+            Console.SetCursorPosition(2,12);
+            Console.WriteLine("+------------------------------------------------+");
+        }
+
+        //Dibuja las opcioness
+
+        public static void DibujarOpciones(int k)
+        { 
+            ConsoleColor cc = ConsoleColor.White;
+            ConsoleColor sel = ConsoleColor.Green;
+
+            Console.SetCursorPosition(20,5);
+            Console.ForegroundColor = k == 0 ? sel : cc;
+            Console.WriteLine("Juego 1 ");
+            Console.SetCursorPosition(20,6);
+            Console.ForegroundColor = k == 1 ? sel : cc;
+            Console.WriteLine("Juego 2");
+            Console.SetCursorPosition(20,7);
+            Console.ForegroundColor = k == 2 ? sel : cc;
+            Console.WriteLine("Juego 3");
+            Console.SetCursorPosition(20,8);
+            Console.ForegroundColor = k == 3 ? sel : cc;
+            Console.WriteLine("Todos");
+            Console.SetCursorPosition(20,9);
+            Console.ForegroundColor = k == 4 ? sel : cc;
+            Console.WriteLine("Salir ");
+        }
+
+        //Dibuja las opciones despues de un juego 
+
+        public static void DibujarRetorno(int n, int left, int top)
         {
-            Console.WriteLine("+------------------------------------------------+");
-            Console.WriteLine("+------------------------------------------------+");
+            ConsoleColor cc = ConsoleColor.White;
+            ConsoleColor sel = ConsoleColor.Green;
+
+            Console.SetCursorPosition(left,top + 1);
+            Console.ForegroundColor = n == 0 ? sel : cc;
+            Console.WriteLine("Volver a Jugar");
+            Console.SetCursorPosition(left + 50, top + 1);
+            Console.ForegroundColor = n == 1 ? sel : cc;
+            Console.WriteLine("Salir");
+        }
+
+        // Codigo para volver a jugar/salir
+
+        public static bool Retorno( int left, int top)
+        {
             
+            bool ejecutar = false;
+           
+            for(int n = 0; ;) { 
+                DibujarRetorno(n, left, top);
+                ConsoleKeyInfo cki = Console.ReadKey(true);  
+                switch (cki.Key)
+                {
+                       case ConsoleKey.LeftArrow: n--; break;
+                       case ConsoleKey.RightArrow: n++; break;
+                       case ConsoleKey.Enter: ejecutar = true; break;
+                }
+                if (n < 0) n = 1; 
+                else if (n > 1) n = 0;
+
+                if (ejecutar) { 
+                    ejecutar = false;
+                    switch (n)
+                    {
+                          case 0: return true;
+                          case 1: return false;
+                    }
+                }
+            }           
         }
 }
